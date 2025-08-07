2d0de1907322c724277f40839392fbee801453d3
Gaurav <gauravkumar.eng@gmail.com>
2025-08-07 22:01:12 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.23.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pulling the latest nginx image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create and run nginx container
+resource "docker_container" "nginx_container" {
+  name  = "nginx-terraform"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 50
+    external = 5050
+  }
+}
